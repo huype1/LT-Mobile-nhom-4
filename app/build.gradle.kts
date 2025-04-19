afe75a4531be4506b79fce622e32c64bbc10f4a0
Bao-gio-co-tien <129248299+Bao-gio-co-tien@users.noreply.github.com>
2025-04-19 23:38:39 +0700

    Camera layout added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    val camerax_version = "1.2.2"
+    implementation("androidx.camera:camera-core:$camerax_version")
+    implementation("androidx.camera:camera-camera2:$camerax_version")
+    implementation("androidx.camera:camera-lifecycle:$camerax_version")
+    implementation("androidx.camera:camera-video:$camerax_version")
+    implementation("androidx.camera:camera-view:$camerax_version")
+    implementation("androidx.camera:camera-extensions:$camerax_version")
 }
